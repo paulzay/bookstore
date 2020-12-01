c34e3e573c1e949cadc4e50ef3d786a5575f24e9
paulzay <paulotieno2@gmail.com>
2020-12-01 22:07:48 +0300

    wrap up store config

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import configureStore from './redux/configStore';
 import './index.css';
 import App from './components/App';
 
+const store = configureStore();
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root'),
 );
