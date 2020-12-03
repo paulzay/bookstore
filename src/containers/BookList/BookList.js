56524cc6d7e41dc83fa4eab4dca321a8ae2f5ec7
paulzay <paulotieno2@gmail.com>
2020-12-03 15:34:11 +0300

    add books to redux state

--- src/containers/BookList/BookList.js
+++ src/containers/BookList/BookList.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Book from '../../components/Book/Book';
+import { deleteBook } from '../../redux/actions/index';
 import './booklist.css';
 
-function BookList({ books }) {
+function BookList({ books, deleteBook }) {
   return (
     <table className="table">
       <thead>
@@ -17,7 +18,7 @@ function BookList({ books }) {
       <tbody>
         {
           books.map(book => (
-            <Book key={book.id} book={book} />
+            <Book key={book.id} book={book} deleteBook={deleteBook} />
           ))
         }
       </tbody>
@@ -25,11 +26,19 @@ function BookList({ books }) {
   );
 }
 
+BookList.propTypes = {
+  books: PropTypes.instanceOf(Array).isRequired,
+  deleteBook: PropTypes.func.isRequired,
+};
+
 const mapStateToProps = state => ({
   books: state.books,
 });
 
-BookList.propTypes = {
-  books: PropTypes.instanceOf(Array).isRequired,
-};
-export default connect(mapStateToProps, null)(BookList);
+const mapDispatchToProps = dispatch => ({
+  deleteBook: id => {
+    dispatch(deleteBook(id));
+  },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
