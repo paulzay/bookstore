d704cbf6b3dfd30ba09147906d8ce9f6d70ddc30
paulzay <paulotieno2@gmail.com>
2020-12-03 15:33:35 +0300

    delete books from state

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -3,20 +3,26 @@ import PropTypes from 'prop-types';
 
 import './book.css';
 
-function Book({ book }) {
+function Book({
+  book, deleteBook,
+}) {
   return (
     <tr>
       <td>{book.id}</td>
       <td>{book.title}</td>
       <td>{book.category}</td>
+      <td><button type="submit" onClick={() => { deleteBook(book.id); }}>Delete Book</button></td>
     </tr>
   );
 }
 
 Book.propTypes = {
-  id: PropTypes.number,
-  title: PropTypes.string,
-  category: PropTypes.string,
-}.isRequired;
+  book: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    category: PropTypes.string.isRequired,
+  }).isRequired,
+  deleteBook: PropTypes.func.isRequired,
+};
 
 export default Book;
