e19d8de8804174f2ffbf26c96bbe5c58b6165b09
paulzay <paulotieno2@gmail.com>
2020-12-15 21:57:19 +0300

    build book model schema

---
+++ backend/models/book.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const bookSchema = mongoose.Schema({
+    title: {type: String, required: true },
+    category: {type: String, required: true }
+})
+
+module.exports = mongoose.model('Book', bookSchema);
