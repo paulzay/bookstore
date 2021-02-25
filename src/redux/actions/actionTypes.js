d0b851244cff81e72230f16235839fe71176dcdd
paulzay <paulotieno2@gmail.com>
2021-02-25 23:04:02 +0300

    add action for geting all books

--- src/redux/actions/actionTypes.js
+++ src/redux/actions/actionTypes.js
@@ -2,6 +2,7 @@ const BookActionTypes = {
   CREATE_BOOK: 'CREATE_BOOK',
   REMOVE_BOOK: 'REMOVE_BOOK',
   CHANGE_FILTER: 'CHANGE_FILTER',
+  GET_BOOKS: 'GET_BOOKS',
 };
 
 export default BookActionTypes;
